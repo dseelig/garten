ca195874ad7cfedebec919dd16fa5d1522ea7d93
dseelig <daniel.seelig@slub-dresden.de>
2018-04-24 07:26:20 +0200

    Add git update.

--- update-ip.sh
+++ update-ip.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 ipNew=$(curl -s ip.skittel.de)
 name=$(cat "$1")
+currentdate=date+"%d.%m.%y" 
 
 update_ip_and_git() {
 	echo $ipNew > $1
+	git add ip-raspberry-pi-3
+	git commit ip-raspberry-pi-3 -m"ip update from - $currentdate"
+	git push ip-raspberry-pi-3
 }
 
 if [ -z "$name" ]
